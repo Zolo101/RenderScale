bacf1130406144665d8e0f45b8cf04bd798214ea
Zelo101 <umadeguy@gmail.com>
2026-09-20 16:40:09 +0100

    Fix(DS): Disable DS + Shaders for all versions (temporary... I hope)

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -28,6 +28,7 @@ stonecutter parameters {
     constants["sodium"] = current.parsed >= "1.21.11" || current.version == "1.21.1"
 
     // Versions affected by stuttering with Dynamic Scaling + Iris Shaders
-    constants["stutter"] = current.version == "1.20.1" || current.version == "1.21.1"
+    // TODO: Looks like all versions have this issue :(
+    constants["stutter"] = true//current.version == "1.20.1" || current.version == "1.21.1"
 
 }
